d6864c00f8b8fdf087aa65bbdf9f91e3d1ad80f6
Rishi <34397126+urishiraval@users.noreply.github.com>
2020-11-05 11:53:29 +0200

    Lists retrieval complete

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { Plugin } from "obsidian";
 import runApplescript from "run-applescript";
 
-var scripts = {
-  "get_lists": () => ``,
+const DELIMITER = "|"
+const scripts = {
+  "get_lists": () => `tell application "Reminders"
+                        set _ids to id of lists
+                        set names to name of lists
+                        set colrs to color of lists
+	                      return {_ids, "${DELIMITER}", names, "${DELIMITER}", colrs}
+                      end tell`,
   "add_reminder": () => ``,
   "get_active_reminders": () => ``,
   "remove_reminder": () => ``,
@@ -12,6 +18,26 @@ var scripts = {
 class RemindersInterface {
   ScriptExecutor = runApplescript;
 
+  getLists() {
+    return new Promise((resolve, reject) => {
+      this.ScriptExecutor(scripts["get_lists"]()).then((res => {
+        let raw_list = res.split(",")
+        raw_list.forEach((value, index) => {
+          raw_list[index] = value.trim();
+        })
+
+        let list = [];
+        let seperator = raw_list.findIndex(value => DELIMITER == value)
+        for (let i = 0; i < seperator; ++i) {
+          list.push({ [raw_list[i]]: { name: raw_list[i + seperator + 1], color: raw_list[i + seperator * 2 + 2] } })
+        }
+
+        resolve(list)
+
+      })).catch((err) => reject(err))
+    })
+  }
+
 }
 
 export default class AppleRemindersPlugin extends Plugin {
@@ -20,6 +46,9 @@ export default class AppleRemindersPlugin extends Plugin {
 
   async onload() {
     console.log("Apple Reminders Plugin is Loading...");
+    this.apple.getLists().then((res) => {
+      console.log(res);
+    })
   }
 
   onunload() {

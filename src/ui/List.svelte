5ba37800d2e4f7ac5b078c6600c59a63e08bc76f
Rishi <ushir.raval@gmail.com>
2020-11-08 00:54:08 +0200

    Made UI show query and data for dev purposes

--- src/ui/List.svelte
+++ src/ui/List.svelte
@@ -1,5 +1,18 @@
-<script>
-	export let data;
+<script lang="ts">
+import { QueryString } from 'src/interfaces';
+
+	import { logger } from "src/tools";
+	import AppleApi from "../engine/api";
+	export let data: QueryString;
+	export let root: HTMLElement;
+	let _ = {};
+	AppleApi.submit(data).then((res) => {
+		logger(root, "Result of Query", res);
+		_ = res;
+	});
 </script>
 
-<code>{data}</code>
+<h3>Query</h3>
+<code>{data.query}</code>
+<h3>Data</h3>
+<pre>{JSON.stringify(_, null, 3)}</pre>

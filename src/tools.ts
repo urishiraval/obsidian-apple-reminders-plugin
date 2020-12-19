8f2503b9347d4ae52d52ad00274d5a2f00e9b73a
Rishi <ushir.raval@gmail.com>
2020-12-19 02:24:03 +0200

    Switch off Debug Mode

--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,5 @@
+import { DEBUG_MODE } from "./constants";
+
 export function mapReplacer(key: any, value: any) {
 	if (value instanceof Map) {
 		return Array.from(value.entries())
@@ -6,6 +8,7 @@ export function mapReplacer(key: any, value: any) {
 }
 
 export function logger(source:Object, msg:any, data?:any) {
+	if(!DEBUG_MODE) return;
 	if(data)
 		console.log({
 			source,
